146ce5bf3f4d60e673235ccf383d16e2aa8551f9
Myles Voigt <myles.voigt@gmail.com>
2019-03-06 16:10:47 -0600

    added some todos

--- Music_Finder.py
+++ Music_Finder.py
@@ -1,5 +1,3 @@
 
-#todo make environment variable for musixmatch api
-#todo make environmentvariable for spotify api
 #todo create cache for api responses
 #todo divide up work between team memebers
--- Spotify_Manager.py
+++ Spotify_Manager.py
@@ -1,2 +1,5 @@
 import requests
 import os
+
+#todo make environment variable for spotify api
+key = os.environ.get('environment variable name goes here')
