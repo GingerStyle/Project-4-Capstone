92f03a17a3fa2859197512e60c7b6ea14cd09fa6
Myles Voigt <myles.voigt@gmail.com>
2019-03-06 15:58:45 -0600

    added api manager files and environment variable for eventful api

---
+++ Eventful_Manager.py
@@ -0,0 +1,4 @@
+import requests
+import os
+
+key = os.environ.get('EventfulKey')
---
+++ Music_Finder.py
@@ -0,0 +1,5 @@
+
+#todo make environment variable for musixmatch api
+#todo make environmentvariable for spotify api
+#todo create cache for api responses
+#todo divide up work between team memebers
