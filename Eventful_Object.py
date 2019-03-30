8fcd78ec6fb6b03907d8ff92669ec1568abb6dbf
Myles Voigt <myles.voigt@gmail.com>
2019-03-30 15:14:07 -0500

    added eventful and spotify object

---
+++ Eventful_Object.py
@@ -0,0 +1,13 @@
+#object to hold concert information from eventful api response
+class Eventful_Object:
+
+    #initiator
+    def __init__(self, event, place, venue, date,):
+        self.event = event
+        self.place = place
+        self.venue = venue
+        self.date = date
+
+    #return string
+    def __str__(self):
+        return f'{self.event} is in {self.place} at {self.venue} on {self.date}'
