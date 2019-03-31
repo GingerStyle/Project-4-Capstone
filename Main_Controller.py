afac01e74dcb9e5068de2b790c8b9c9f8e14737f
Nathan W <nathan@email.com>
2019-03-31 11:02:41 -0500

    setup cache using requests_cache and created three classes/functions to figure out how to process the search, results, caching.

--- Main_Controller.py
+++ Main_Controller.py
@@ -1,3 +1,17 @@
+import requests
+import requests_cache
+from UI.py import *
 
 #todo create cache for api responses
 #todo divide up work between team members
+
+requests_cache.install_cache('github_cache', backend='sqlite', expire_after=180)
+
+class send_seasrch_to_api():
+    #This class will send the search from UI.py to API_Manager.py
+
+class retrieve_and_cache_results():
+    #This class will receive the search results then cahce them
+
+class save_search_results():
+    #this class will take the search results and save them if the user would like
